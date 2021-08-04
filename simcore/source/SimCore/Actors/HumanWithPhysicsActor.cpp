d413b9b8a64ea0e63e971d09f41e1c8a6ac025c5
王浩 <deverwh@outlook.com>
2021-08-04 15:23:35 +0800

    回退到2.8.0可编译版

---
+++ simcore/source/SimCore/Actors/HumanWithPhysicsActor.cpp
@@ -0,0 +1,225 @@
+/* -*-c++-*-
+* Simulation Core
+* Copyright 2007-2008, Alion Science and Technology
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* This software was developed by Alion Science and Technology Corporation under
+* circumstances in which the U. S. Government may have rights in the software.
+ * @author Eddie Johnson
+ */
+#include <prefix/SimCorePrefix.h>
+#include <SimCore/Actors/HumanWithPhysicsActor.h>
+
+#include <SimCore/Messages.h>
+#include <SimCore/MessageType.h>
+
+#include <dtCore/enginepropertytypes.h>
+
+#include <dtGame/invokable.h>
+#include <dtGame/basemessages.h>
+#include <dtGame/deadreckoninghelper.h>
+
+#include <dtCore/keyboard.h>
+#include <dtCore/batchisector.h>
+
+#include <dtABC/application.h>
+
+#include <dtUtil/matrixutil.h>
+#include <dtUtil/mathdefines.h>
+
+#include <SimCore/Actors/PagedTerrainPhysicsActor.h>
+
+#include <SimCore/CollisionGroupEnum.h>
+
+
+namespace SimCore
+{
+   namespace Actors
+   {
+      //////////////////////////////////////////////////////////
+      // Actor code
+      //////////////////////////////////////////////////////////
+      HumanWithPhysicsActor::HumanWithPhysicsActor(dtGame::GameActorProxy& owner)
+      : Human(owner)
+      // With 30, this is about 12.66 MPH, which is a decently fast sustainable run. The fastest human sprint
+      // speed is like 27 MPH. Typically slow walk speed is like 3 MPH. A marathoner can sustain 12.55 MPH
+      // for 2 hours. Note, this multiplies times the frame speed using the motion model, but it should be
+      // irrelevant of FPS.
+      , mMoveRateConstant(30.0f, 30.0f, 0.0f)
+      , mNotifyChangePosition(false)
+      , mNotifyChangeOrient(false)
+      , mNotifyChangeVelocity(false)
+      {
+      }
+
+      ////////////////////////////////////////////////////////////
+      HumanWithPhysicsActor::~HumanWithPhysicsActor()
+      {
+      }
+
+      ////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::OnTickLocal(const dtGame::TickMessage& tickMessage)
+      {
+         mNotifyChangePosition = false;
+         mNotifyChangeOrient = false;
+         mNotifyChangeVelocity = false;
+
+         //TODO: CURRENTLY NO LOCAL BEHAVIOR WITH dtPhysics in local mode
+
+
+         Human::OnTickLocal(tickMessage);
+      }
+
+      ////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::OnTickRemote(const dtGame::TickMessage& tickMessage)
+      {
+         Human::OnTickRemote(tickMessage);
+
+         dtCore::Transform transform;
+         GetTransform(transform);
+
+         transform.GetTranslation(mPreviousTransform);
+      }
+
+      ////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::SetPosition( const osg::Vec3& position )
+      {
+         dtCore::Transform xform;
+         xform.SetTranslation( position );
+         SetTransform(xform);
+      }
+
+      ////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::OffsetPosition( const osg::Vec3& offset )
+      {
+         dtCore::Transform xform;
+         GetTransform( xform );
+         osg::Vec3 trans;
+         xform.GetTranslation(trans);
+         trans += offset;
+         xform.SetTranslation( trans );
+         SetPosition( trans );
+      }
+
+      ////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::OnEnteredWorld()
+      {
+         Human::OnEnteredWorld();
+
+         GetPhysicsActComp()->SetPrePhysicsCallback(dtPhysics::PhysicsActComp::UpdateCallback(this, &HumanWithPhysicsActor::PrePhysicsUpdate));
+
+         dtPhysics::PhysicsObject* po = GetPhysicsActComp()->GetMainPhysicsObject();
+         // Human remote is a bad group name because the local behavior is the same.
+         po->SetCollisionGroup(CollisionGroup::GROUP_HUMAN_REMOTE);
+
+         // If the developer didn't set the origin offset to something.
+         // This is sort of a problem, because if the user WANTS no offset, they would have to set it to
+         // something really small, but not quite 0.
+         if (po->GetOriginOffset().length2() < FLT_EPSILON)
+         {
+            osg::Vec3 extents = po->GetExtents();
+            // Move the cylinder up half the height to sync up the origins.
+            po->SetOriginOffset(osg::Vec3(0.0f, 0.0f, (extents.x() / 2.0f) + extents.y()));
+         }
+
+
+         po->Create();
+      }
+
+      ////////////////////////////////////////////////////////////////////
+      dtPhysics::PhysicsActComp* HumanWithPhysicsActor::GetPhysicsActComp()
+      {
+         dtPhysics::PhysicsActComp* physAC = NULL;
+         GetComponent(physAC);
+         return physAC;
+      }
+
+
+      ////////////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::PrePhysicsUpdate()
+      {
+          dtCore::Transform xform;
+          GetTransform(xform);
+          dtPhysics::PhysicsObject* physObj = GetPhysicsActComp()->GetMainPhysicsObject();
+          if (physObj != NULL)
+          {
+             physObj->SetTransformAsVisual(xform);
+          }
+      }
+
+      ///////////////////////////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActor::PostPhysicsUpdate()
+      {
+         if (!IsRemote())
+         {
+         }
+      }
+
+      //////////////////////////////////////////////////////////
+      // Proxy code
+      //////////////////////////////////////////////////////////
+      HumanWithPhysicsActorProxy::HumanWithPhysicsActorProxy()
+      {
+         SetClassName("HumanWithPhysicsActor");
+      }
+
+      //////////////////////////////////////////////////////////////////////////
+      HumanWithPhysicsActorProxy::~HumanWithPhysicsActorProxy()
+      {
+
+      }
+
+      /// Instantiates the actor this proxy encapsulated
+      void HumanWithPhysicsActorProxy::CreateDrawable()
+      {
+         HumanWithPhysicsActor* p = new HumanWithPhysicsActor(*this);
+         SetDrawable(*p);
+      }
+
+      //////////////////////////////////////////////////////////////////////
+      void HumanWithPhysicsActorProxy::BuildActorComponents()
+      {
+         if (!HasComponent(dtPhysics::PhysicsActComp::TYPE))
+         {
+            dtCore::RefPtr<dtPhysics::PhysicsActComp> physAC = new dtPhysics::PhysicsActComp();
+
+            dtCore::RefPtr<dtPhysics::PhysicsObject> physicsObject = new dtPhysics::PhysicsObject("Body");
+            physicsObject->SetPrimitiveType(dtPhysics::PrimitiveType::CYLINDER);
+            physicsObject->SetMechanicsType(dtPhysics::MechanicsType::KINEMATIC);
+            physicsObject->SetCollisionGroup(SimCore::CollisionGroup::GROUP_HUMAN_LOCAL);
+            physicsObject->SetMass(100.0f);
+            physicsObject->SetExtents(osg::Vec3(1.8f, 0.5f, 0.0f));
+            physAC->AddPhysicsObject(*physicsObject);
+
+            AddComponent(*physAC);
+         }
+
+         BaseClass::BuildActorComponents();
+
+         dtCore::RefPtr<dtGame::DeadReckoningHelper> drAC;
+         GetComponent(drAC);
+         if (drAC.valid())
+         {
+            // We don't want the human to lean sideways, regardless of what is sent. It looks stupid
+            drAC->SetForceUprightRotation(true);
+            // default to velocity only.  Humans walk.
+            drAC->SetDeadReckoningAlgorithm(dtGame::DeadReckoningAlgorithm::VELOCITY_ONLY);
+         }
+
+      }
+
+   }
+}
---
+++ simcore/source/SimCore/Actors/ViewerMaterialActor.cpp
@@ -0,0 +1,201 @@
+/* -*-c++-*-
+* Simulation Core
+* Copyright 2007-2008, Alion Science and Technology
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* This software was developed by Alion Science and Technology Corporation under
+* circumstances in which the U. S. Government may have rights in the software.
+* @author Allen Danklefsen
+*/
+
+#include <prefix/SimCorePrefix.h>
+#include <SimCore/Actors/ViewerMaterialActor.h>
+#include <dtCore/enginepropertytypes.h>
+#include <dtCore/functor.h>
+#include <SimCore/Components/ViewerMaterialComponent.h>
+
+namespace SimCore
+{
+   namespace Actors
+   {
+      //////////////////////////////////////////////////////////
+      // Proxy code
+      //////////////////////////////////////////////////////////
+
+      //////////////////////////////////////////////////////////
+      ViewerMaterialActorProxy::ViewerMaterialActorProxy()
+      {
+         SetClassName("ViewerMaterialActor");
+
+      }
+
+      //////////////////////////////////////////////////////////
+      ViewerMaterialActorProxy::~ViewerMaterialActorProxy()
+      {
+
+      }
+
+      //////////////////////////////////////////////////////////
+      void ViewerMaterialActorProxy::BuildPropertyMap()
+      {
+         const std::string GROUP       = "Not Categorized Material Types";
+         const std::string PHYSGROUP   = "Physics Material Types";
+         const std::string SHADGROUP   = "Shader Material Types";
+         const std::string PARTGROUP   = "Particle Systems Material Types";
+         const std::string SFXGROUP    = "Sound Effects Material Types";
+         const std::string MUSCGROUP   = "Music Material Types";
+         const std::string NETGROUP    = "Networked Material Types";
+         const std::string VISLGROUP   = "Visual Material Types";
+         const std::string OTHRGROUP   = "Other Types or Multiple Material Types";
+
+         ViewerMaterialActor *actor = dynamic_cast<ViewerMaterialActor*>(GetDrawable());
+
+         AddProperty(new dtCore::ColorRgbaActorProperty("Base Color Value", "Base Color Value",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetBaseColorvalue),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetBaseColorvalue),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::ColorRgbaActorProperty("Highlight Color Value", "Highlight Color Value",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetHighlighteColorvalue),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetHighlighteColorvalue),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::FloatActorProperty("DynamicFriction", "DynamicFriction",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetDynamicFriction),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetDynamicFriction),
+            "Ageia Material Setting - coefficient of dynamic friction -- should be in [0, +inf]. If set to greater than staticFriction, the effective value of staticFriction will be increased to match. if flags & NX_MF_ANISOTROPIC is set, then this value is used for the primary direction of anisotropy (U axis)", PHYSGROUP));
+
+         AddProperty(new dtCore::FloatActorProperty("StaticFriction", "StaticFriction",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetStaticFriction),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetStaticFriction),
+            "Ageia Material Setting - coefficient of static friction -- should be in [0, +inf] if flags & NX_MF_ANISOTROPIC is set, then this value is used for the primary direction of anisotropy (U axis)", PHYSGROUP));
+
+         AddProperty(new dtCore::FloatActorProperty("Restitution", "Restitution",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetRestitution),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetRestitution),
+            "Ageia Material Setting - coefficient of restitution -- 0 makes the object bounce as little as possible, higher values up to 1.0 result in more bounce. Note that values close to or above 1 may cause stability problems and/or increasing energy. Range: [0,1]", PHYSGROUP));
+
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::SOUND,
+            "SmallHitSoundEffect", "SmallHitSoundEffect",  dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetSmallHitSoundEffect),
+            "", SFXGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::SOUND, "MediumHitSoundEffect", "MediumHitSoundEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetMediumHitSoundEffect),
+            "", SFXGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::SOUND, "LargeHitSoundEffect", "LargeHitSoundEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetLargeHitSoundEffect),
+            "", SFXGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::SOUND, "AmbientSoundEffect", "AmbientSoundEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetAmbientSoundEffect),
+            "", SFXGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::SOUND, "AmbientMusicEffect", "AmbientMusicEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetAmbientMusicEffect),
+            "", MUSCGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::TEXTURE, "DecalSmallHit", "DecalSmallHit",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetDecalSmallHit),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::TEXTURE, "DecalMediumHit", "DecalMediumHit",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetDecalMediumHit),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::TEXTURE, "DecalLargeHit", "DecalLargeHit",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetDecalLargeHit),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::PARTICLE_SYSTEM, "DustTrailEffect", "DustTrailEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetDustTrailEffect),
+            "", PARTGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::PARTICLE_SYSTEM, "SmallHitEffect", "SmallHitEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetSmallHitEffect),
+            "", PARTGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::PARTICLE_SYSTEM, "MediumHitEffect", "MediumHitEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetMediumHitEffect),
+            "", PARTGROUP));
+
+         AddProperty(new dtCore::ResourceActorProperty(*this, dtCore::DataType::PARTICLE_SYSTEM, "LargeHitEffect", "LargeHitEffect",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetLargeHitEffect),
+            "", PARTGROUP));
+
+         AddProperty(new dtCore::StringActorProperty("PhysicsParticleLookupStringOne", "PhysicsParticleLookupStringOne",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetPhysicsParticleLookupStringOne),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetPhysicsParticleLookupStringOne),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::StringActorProperty("PhysicsParticleLookupStringTwo", "PhysicsParticleLookupStringTwo",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetPhysicsParticleLookupStringTwo),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetPhysicsParticleLookupStringTwo),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::StringActorProperty("PhysicsParticleLookupStringThr", "PhysicsParticleLookupStringThr",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetPhysicsParticleLookupStringThr),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetPhysicsParticleLookupStringThr),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::StringActorProperty("PhysicsParticleLookupStringFour", "PhysicsParticleLookupStringFour",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetPhysicsParticleLookupStringFour),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetPhysicsParticleLookupStringFour),
+            "", VISLGROUP));
+
+         AddProperty(new dtCore::StringActorProperty("PhysicsParticleLookupStringFive", "PhysicsParticleLookupStringFive",
+            dtCore::MakeFunctor(*actor, &ViewerMaterialActor::SetPhysicsParticleLookupStringFive),
+            dtCore::MakeFunctorRet(*actor, &ViewerMaterialActor::GetPhysicsParticleLookupStringFive),
+            "", VISLGROUP));
+
+      }
+
+      // Creates the actor
+      void ViewerMaterialActorProxy::CreateDrawable()
+      {
+         SetDrawable(*new ViewerMaterialActor(*this));
+      }
+
+      //////////////////////////////////////////////////////////
+      // Actor code
+      //////////////////////////////////////////////////////////
+
+      //////////////////////////////////////////////////////////
+      ViewerMaterialActor::ViewerMaterialActor(dtGame::GameActorProxy& owner) : dtGame::GameActor(owner)
+         , m_PHYS_DynamicFriction(0.5f)
+         , m_PHYS_StaticFriction(0.5f)
+         , m_PHYS_Restitution(0.2f)
+      {
+         SetPhysicsParticleLookupStringOne("");
+         SetPhysicsParticleLookupStringTwo("");
+         SetPhysicsParticleLookupStringThr("");
+         SetPhysicsParticleLookupStringFour("");
+         SetPhysicsParticleLookupStringFive("");
+      }
+
+      void ViewerMaterialActor::OnEnteredWorld()
+      {
+         SimCore::Components::ViewerMaterialComponent* materialComponent = dynamic_cast<SimCore::Components::ViewerMaterialComponent*>(GetGameActorProxy().GetGameManager()->GetComponentByName("ViewerMaterialComponent"));
+         if(materialComponent == NULL)
+         {
+            LOG_ERROR("materialComponent Is not initialized, make sure a new one was made before loading a map in");
+         }
+         else
+            materialComponent->RegisterAMaterialWithComponent(this);
+      }
+   }
+}
